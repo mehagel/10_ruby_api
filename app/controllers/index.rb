63c5a1da65553dd089ed4243b324b378ca7dc85b
Apprentice <apprentice@devbootcamp.com>
2013-07-30 19:38:54 -0700

    updated files

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -2,3 +2,10 @@ get '/' do
   # Look in app/views/index.erb
   erb :index
 end
+
+get '/username' do
+
+  @tweets = Twitter.user_timeline(params[:name], count: params[:count])
+
+  erb :list_tweets
+end
